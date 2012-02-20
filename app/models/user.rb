ae2be30337d53ea726094e52b7e45a2f866dfffc
Amina Bouabdallah <amina.bouabdallah@gmail.com>
2012-02-19 22:26:59 -0500

    removed sex fields from the db

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email, :password, :password_confirmation, :sex
+  attr_accessible :name, :email, :password, :password_confirmation, 
   has_secure_password
 
   validates :name, presence: true, length: { maximum: 50 }
