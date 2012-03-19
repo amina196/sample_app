866735c8a700d7a1700b4ccf4e6e89344d4d7d2f
Amina Bouabdallah <amina.bouabdallah@gmail.com>
2012-03-18 22:23:45 -0500

    Fixed the error syntax due to PostGres

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,7 +5,7 @@ class Micropost < ActiveRecord::Base
 	default_scope order: 'microposts.created_at DESC' #order the microposts, most recent first
 	
 	def self.from_users_followed_by(user)
-    followed_user_ids = user.followed_user_ids.join(', ')
+    followed_user_ids = user.followed_user_ids
     where("user_id IN (?) OR user_id = ?", followed_user_ids, user)
   end
 end
