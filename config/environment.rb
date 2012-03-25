e38117984fc09d799b9eb474bc58114ff128c6ca
Amina Bouabdallah <amina.bouabdallah@gmail.com>
2012-03-25 17:34:28 -0400

    :q!

--- config/environment.rb
+++ config/environment.rb
@@ -4,15 +4,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 SampleApp::Application.initialize!
 
-# Configuration for using SendGrid on Heroku
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  :user_name => ENV["SENDGRID_USERNAME"]
-  :password => ENV["SENDGRID_PASSWORD"],
-  :domain => "heroku.com",
-  :address => "smtp.sendgrid.net",
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
 }
 
