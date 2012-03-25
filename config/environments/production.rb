eb9794a6e1418b97e0e4f5f2d0cc1d63c1826066
Amina Bouabdallah <amina.bouabdallah@gmail.com>
2012-03-25 17:05:07 -0400

    Deleted the lines added in production.rb, following online setup suggestions

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,15 +58,4 @@ SampleApp::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.action_mailer.smtp_settings = {
-	:address => 'smtp.sendgrid.net',
-	:port => '587',
-	:authentication => :plain,
-	:user_name => ENV['SENDGRID_USERNAME'],
-	:password => ENV['SENDGRID_PASSWORD'],
-	:domain => 'heroku.com'
-  }
   
-config.action_mailer.default_url_options
-{ :host => "turoring.herokuapp.com"]
-end
