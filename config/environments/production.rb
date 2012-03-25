2a4a3a2e1627d0aa2d5a946db66a874bec4a9867
Amina Bouabdallah <amina.bouabdallah@gmail.com>
2012-03-25 17:45:56 -0400

    :q

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-SampleApp::Application.configure do
+DemoApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
@@ -57,5 +57,4 @@ SampleApp::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-  
- end
+end
