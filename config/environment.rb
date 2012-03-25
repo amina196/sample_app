c87ce6d5333c23c0a7682474cc44c5497fa1ffea
Amina Bouabdallah <amina.bouabdallah@gmail.com>
2012-03-25 16:47:28 -0400

    Fixing problem with setting up Sendgrid with StackOverflow

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 SampleApp::Application.initialize!
+
+# Configuration for using SendGrid on Heroku
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :user_name => "app2995107@heroku.com",
+  :password => "tcxbw2u3",
+  :domain => "tutoring.herokuapp.com",
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
