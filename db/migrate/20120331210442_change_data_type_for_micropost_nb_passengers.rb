5077906d5b56ff3e819517020471725222f7bcd1
Amina Bouabdallah <amina.bouabdallah@gmail.com>
2012-05-04 11:20:09 -0400

    got rid of unworking migrations

--- db/migrate/20120331210442_change_data_type_for_micropost_nb_passengers.rb
+++ db/migrate/20120331210442_change_data_type_for_micropost_nb_passengers.rb
@@ -1,9 +1,4 @@
 class ChangeDataTypeForMicropostNbPassengers < ActiveRecord::Migration
   def change
-  	change_table :microposts do |t|
-      t.change :nb_passengers, :integer
-    end
-  end
-
-  
+  	
 end
